52e1eb8dd8351cc2a6e92b22c084494f6c04be6f
sundeep kumar <sundeepkr007@gmail.com>
2023-04-05 15:14:13 +0530

    commited

---
+++ src/test/java/test/Test123.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Test123 {
+
+	@Test
+	public void Test07Test(){
+		
+		System.out.println("i am adding one new class");
+
+	}
+
+}
